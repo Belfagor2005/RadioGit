0f7348a42033fd656a762e26c4e99f254c08ac52
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2026-04-04 16:39:58 +0200

    add autotag

--- usr/lib/enigma2/python/Plugins/Extensions/RadioGit/__init__.py
+++ usr/lib/enigma2/python/Plugins/Extensions/RadioGit/__init__.py
@@ -4,7 +4,7 @@ __author__ = "Lululla"
 __email__ = "ekekaz@gmail.com"
 __copyright__ = 'Copyright (c) 2024 Lululla'
 __license__ = "GPL-v2"
-__version__ = "1.0.0"
+__version__ = "1.2"
 
 from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS
@@ -18,32 +18,27 @@ PluginLanguagePath = 'Extensions/RadioGit/locale'
 
 isDreambox = False
 if os.path.exists("/usr/bin/apt-get"):
-    isDreambox = True
+	isDreambox = True
 
 
 def localeInit():
-    if isDreambox:
-        lang = language.getLanguage()[:2]
-        os.environ["LANGUAGE"] = lang
-    gettext.bindtextdomain(
-        PluginLanguageDomain,
-        resolveFilename(
-            SCOPE_PLUGINS,
-            PluginLanguagePath))
+	if isDreambox:
+		lang = language.getLanguage()[:2]
+		os.environ["LANGUAGE"] = lang
+	gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
 
 
 if isDreambox:
-    def _(txt):
-        return gettext.dgettext(PluginLanguageDomain, txt) if txt else ""
+	def _(txt):
+		return gettext.dgettext(PluginLanguageDomain, txt) if txt else ""
 else:
-    def _(txt):
-        translated = gettext.dgettext(PluginLanguageDomain, txt)
-        if translated:
-            return translated
-        else:
-            print(("[%s] fallback to default translation for %s" %
-                  (PluginLanguageDomain, txt)))
-            return gettext.gettext(txt)
+	def _(txt):
+		translated = gettext.dgettext(PluginLanguageDomain, txt)
+		if translated:
+			return translated
+		else:
+			print(("[%s] fallback to default translation for %s" % (PluginLanguageDomain, txt)))
+			return gettext.gettext(txt)
 
 localeInit()
 language.addCallback(localeInit)
